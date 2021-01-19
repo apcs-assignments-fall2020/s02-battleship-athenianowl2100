679b4d8bae3aeb5f4d99220015784d7f6dfb281f
athenianowl2100 <nathanlintest@gmail.com>
2021-01-19 19:18:50 +0000

    first draft

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,14 @@ public class MyMain {
     // was a boat the specified coordinates. This
     // method also prints out an appropriate message
     public static boolean hit(boolean[][] board, int row, int col) { 
-        // YOUR CODE HERE
-        return false;
+        if (board[row][col]) {
+            System.out.println("There was a hit!");
+            return true;
+        }
+        else {
+            System.out.println("You missed a boat!");
+            return false;     
+        }
     }
 
 
@@ -13,7 +19,17 @@ public class MyMain {
     // The remaining pieces are placed in the direction given
     // by the direction input
     public static boolean[][] placeBoat(boolean[][] board, String direction, int boatLength, int row, int col) { 
-        // YOUR CODE HERE
+        if (direction.equals("down")) {
+            for (int i = 0; i < boatLength; i++) {
+                board[row+i][col] = true;
+            }
+        }
+        else {
+            for (int i = 0; i < boatLength; i++) {
+                board[row][col+i] = true;
+            }
+        }
+
         return board;
     }
 
@@ -22,8 +38,22 @@ public class MyMain {
     // increasing length
     // You may assume that all Strings are lowercase 
     public static boolean inOrder(String[][] words) { 
-        // YOUR CODE HERE
-        return false;
+        for (int row = 0; row < words.length; row++) {
+            for (int col = 0; col < words[0].length-1; col++) {
+                String word1 = words[row][col];
+                String word2 = words[row][col+1];
+
+                if (word2.length() < word1.length()) {
+                    return false;
+                }
+
+                if (word2.compareTo(word1) < 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
